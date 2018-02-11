646025012808f6e94576213545af017aab466d0f
Chris Jannenga <cjannenga312@gmail.com>
2018-02-11 09:02:53 -0700

    MAde a couple nav changes

--- src/App.js
+++ src/App.js
@@ -6,33 +6,50 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header
-            mdl-layout--fixed-tabs">
-  <header className="mdl-layout__header">
+
+
+<div className="demo-layout-waterfall mdl-layout mdl-js-layout">
+  <header className="mdl-layout__header mdl-layout__header--waterfall our-blue">
+
     <div className="mdl-layout__header-row">
 
       <span className="mdl-layout-title">Title</span>
+      <div className="mdl-layout-spacer"></div>
+      <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable
+                  mdl-textfield--floating-label mdl-textfield--align-right">
+        <label className="mdl-button mdl-js-button mdl-button--icon"
+               for="waterfall-exp">
+          <i className="material-icons">search</i>
+        </label>
+        <div className="mdl-textfield__expandable-holder">
+          <input className="mdl-textfield__input" type="text" name="sample"
+                 id="waterfall-exp" />
+        </div>
+      </div>
     </div>
 
-    <div className="mdl-layout__tab-bar mdl-js-ripple-effect">
-      <a href="#fixed-tab-1" className="mdl-layout__tab is-active">Tab 1</a>
-      <a href="#fixed-tab-2" className="mdl-layout__tab">Tab 2</a>
-      <a href="#fixed-tab-3" className="mdl-layout__tab">Tab 3</a>
+    <div className="mdl-layout__header-row">
+      <div className="mdl-layout-spacer"></div>
+
+      <nav className="mdl-navigation">
+        <a className="mdl-navigation__link" href="">Link</a>
+        <a className="mdl-navigation__link" href="">Link</a>
+        <a className="mdl-navigation__link" href="">Link</a>
+        <a className="mdl-navigation__link" href="">Link</a>
+      </nav>
     </div>
   </header>
   <div className="mdl-layout__drawer">
     <span className="mdl-layout-title">Title</span>
+    <nav className="mdl-navigation">
+      <a className="mdl-navigation__link" href="">Link</a>
+      <a className="mdl-navigation__link" href="">Link</a>
+      <a className="mdl-navigation__link" href="">Link</a>
+      <a className="mdl-navigation__link" href="">Link</a>
+    </nav>
   </div>
   <main className="mdl-layout__content">
-    <section className="mdl-layout__tab-panel is-active" id="fixed-tab-1">
-      <div className="page-content">Tab 1</div>
-    </section>
-    <section className="mdl-layout__tab-panel" id="fixed-tab-2">
-      <div className="page-content">Tab 2</div>
-    </section>
-    <section className="mdl-layout__tab-panel" id="fixed-tab-3">
-      <div className="page-content">Tab 3</div>
-    </section>
+    <div className="page-content">Hello</div>
   </main>
 </div>
       </div>
